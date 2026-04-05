b9fc9b8725e1d4a9d6884a5c693cc98c16bfd8bd
slihoel <slihoel71@gmail.com>
2026-04-05 14:40:07 +0800

    draw button

---
+++ Draw.py
@@ -0,0 +1,11 @@
+import pygame
+
+def Button(id, name, surface, rect):
+    try:
+        print(f"draw {name} button")
+        buttonImage = pygame.image.load(f"assets/{name}{id}.png")
+        buttonRect = surface.blit(buttonImage, rect)
+    except pygame.error as e:
+        print(f"Error loading button image for {name}{id}: {e}")
+        return False
+    return True
